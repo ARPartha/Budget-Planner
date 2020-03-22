6969620177f7717a69f8460412580f41e4cd800f
My Name <example@email.com>
2020-03-22 16:27:31 +0600

    dynamic view added

--- app/src/main/java/com/example/budgetplanner/Category.java
+++ app/src/main/java/com/example/budgetplanner/Category.java
@@ -2,13 +2,33 @@ package com.example.budgetplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class Category extends AppCompatActivity {
+    private GridLayout gridLayout;
+    Button addItems;
+    DynamicViews dynamicViews;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+        addItems = (Button)findViewById(R.id.category);
+
+        addItems.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                dynamicViews = new DynamicViews(context);
+                gridLayout.addView(dynamicViews.descriptionTextView(getApplicationContext(),"Item 1"),2);
+                gridLayout.addView(dynamicViews.recievedQuantityEditText(getApplicationContext()),3);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/budgetplanner/DynamicViews.java
+++ app/src/main/java/com/example/budgetplanner/DynamicViews.java
@@ -1,4 +1,41 @@
 package com.example.budgetplanner;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.text.InputType;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class DynamicViews {
+    Context context;
+
+    public DynamicViews(Context context) {
+        this.context = context;
+    }
+
+    public EditText descriptionTextView(Context context,String text)
+    {
+        final ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        final EditText editText = new EditText(context);
+        int id = 0;
+        editText.setId(id);
+        editText.setMinEms(2);
+        editText.setTextColor(Color.rgb(255,255,255));
+        editText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        return editText;
+    }
+
+    public EditText recievedQuantityEditText(Context context){
+        final ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        final EditText editText = new EditText(context);
+        int id = 0;
+        editText.setId(id);
+        editText.setMinEms(2);
+        editText.setTextColor(Color.rgb(255,255,255));
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        return editText;
+    }
 }
